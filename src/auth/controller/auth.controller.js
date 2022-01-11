c3746d325e47eba6e2ae7271dd5ac384fbe9a2e1
Mazen <mazenaboelanin@gmail.com>
2022-01-11 19:51:48 +0200

    creating register controller in auth

--- src/auth/controller/auth.controller.js
+++ src/auth/controller/auth.controller.js
@@ -3,7 +3,22 @@ const User = require('../../users/model/user.model');
 
 
 exports.register = async(req, res, next)=>{
-    res.status(StatusCodes.OK).json({success: true});
+   const {name, email, password, phone, role} = req.body;
+
+   try {
+       const user = await User.findOne({email});
+       if(user){
+        res.status(StatusCodes.BAD_REQUEST).json({ success: false, data: {}, message: "Email already Exists"});
+       }
+       else {
+           const newUser = new User({name, email, password, phone, role});
+           const data = await newUser.save();
+          
+           res.status(StatusCodes.CREATED).json({ success: true, data});
+       }
+   } catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, data: {}, err});
+   }
 }
 
 
